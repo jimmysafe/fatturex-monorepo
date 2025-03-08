ba2dc37c840a548571d7fdff1762bc833c43f1fc
Gianmarco Ciaffardini <ciaffardini.g@gmail.com>
2025-03-08 18:28:53 +0100

    fix: pass6

--- packages/auth/src/lib/send-otp-email.ts
+++ packages/auth/src/lib/send-otp-email.ts
@@ -1,25 +1,26 @@
 /* eslint-disable node/no-process-env */
-// import { SigninInOtpEmail } from "@repo/ui/components/email/signin-otp";
-// import { Resend } from "resend";
-// const resend = new Resend(process.env.RESEND_API_KEY);
+import { SigninInOtpEmail } from "@repo/ui/components/email/signin-otp";
+import { Resend } from "resend";
 
-export async function sendOtpSigninEmail(_: string, otp: string) {
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function sendOtpSigninEmail(email: string, otp: string) {
   try {
     if (process.env.APP_ENV !== "production") {
       // eslint-disable-next-line no-console
       console.log(`======== SIGNIN OTP: ${otp} ========`);
       return { success: true };
     }
-    // const { data, error } = await resend.emails.send({
-    //   from: "Fatturex <info@basilico.studio>",
-    //   to: [email],
-    //   subject: "Fatturex - Il tuo codice di accesso OTP",
-    //   react: SigninInOtpEmail({ otp }),
-    // });
-    // if (error) {
-    //   throw new Error("Errore nell'invio della mail");
-    // }
-    // return data;
+    const { data, error } = await resend.emails.send({
+      from: "Fatturex <info@basilico.studio>",
+      to: [email],
+      subject: "Fatturex - Il tuo codice di accesso OTP",
+      react: SigninInOtpEmail({ otp }),
+    });
+    if (error) {
+      throw new Error("Errore nell'invio della mail");
+    }
+    return data;
   }
   catch (error) {
     throw new Error("Errore nell'invio della mail");
