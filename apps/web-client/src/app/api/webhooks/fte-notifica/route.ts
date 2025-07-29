09f67e7ff8501121e74f9ac787cf3e6540a6add2
Jimmy <ciaffardini.g@gmail.com>
2025-07-29 19:56:19 +0200

    fix: revert back fte wh

--- apps/web-client/src/app/api/webhooks/fte-notifica/route.ts
+++ apps/web-client/src/app/api/webhooks/fte-notifica/route.ts
@@ -69,16 +69,25 @@ export async function POST(request: Request) {
         ? errori.map(e => (e.Suggerimento ?? e.Descrizione)).join("|")
         : errori.Suggerimento ?? errori.Descrizione;
 
-    if (notification.type === "NS") {
-      const existingFattura = await getFattura(notification.invoice_uuid);
-      if (existingFattura?.fteStato === FteStato.INVIATA)
-        return;
+    const existingNotaDiCredito = await getNotaDiCredito(notification.invoice_uuid);
+    const existingFattura = await getFattura(notification.invoice_uuid);
 
-      await updateFattura(
-        notification.invoice_uuid,
-        FteStato.SCARTATA,
-        message,
-      );
+    if (notification.type === "NS") {
+      if (existingFattura) {
+        if (existingFattura?.fteStato === FteStato.INVIATA)
+          return;
+        await updateFattura(
+          notification.invoice_uuid,
+          FteStato.SCARTATA,
+          message,
+        );
+      }
+
+      if (existingNotaDiCredito) {
+        if (existingNotaDiCredito?.fteStato === FteStato.INVIATA)
+          return;
+        await updateNotaDiCredito(notification.invoice_uuid, FteStato.SCARTATA, message);
+      }
 
       //  TODO: SEND SCARTO EMAIL
       // const resend = new Resend(env.RESEND_API_KEY);
@@ -94,10 +103,16 @@ export async function POST(request: Request) {
       // }
     }
     if (notification.type === "RC") {
-      await updateFattura(
-        notification.invoice_uuid,
-        FteStato.INVIATA,
-      );
+      if (existingFattura) {
+        await updateFattura(
+          notification.invoice_uuid,
+          FteStato.INVIATA,
+        );
+      }
+
+      if (existingNotaDiCredito) {
+        await updateNotaDiCredito(notification.invoice_uuid, FteStato.INVIATA);
+      }
     }
   }
 
